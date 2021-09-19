3d9770c76e94d64e09007d46c62f2d491f0645e1
JoelDavidPerez <90626632+JoelDavidPerez@users.noreply.github.com>
2021-09-18 20:01:35 -0500

    Create picasyfijos.cpp

--- picasyfijos.cpp
+++ picasyfijos.cpp
@@ -14,8 +14,8 @@ int main(){
 	//cout<<"Tienes la habilidad para completarlo?\nPresiona __ para continuar"  //buscar como poner botones en c++
 	
 	//Instrucciones
-	cout<<"Instrucciones"
-	cout<<"~Un juego muy conocido es picas y fijas~\n\nconsiste en tratar de adivinar un número en la menor cantidad de intentos.\nEn cada intento, el jugador dice 4 dígitos (no repetidos) y el oponente le da pistas de cuántos aciertos tuvo, sin indicarle cuales fueron, de la siguiente forma:\n Si algún dígito que dice el jugador se encuentra dentro del número a adivinar, pero no está en la posición correcta, se llama PICA.\n Si el dígito se encuentra en la posición adecuada, se llama FIJA.\n Así, las pistas serán la cantidad de PICAS y la cantidad de FIJAS que se tienen.\n El juego termina cuando algún jugador tiene 4 FIJAS (4 dígitos en el orden adecuado)."
+	cout<<"Instrucciones";
+	cout<<"~Un juego muy conocido es picas y fijas~\n\nconsiste en tratar de adivinar un número en la menor cantidad de intentos.\nEn cada intento, el jugador dice 4 dígitos (no repetidos) y el oponente le da pistas de cuántos aciertos tuvo, sin indicarle cuales fueron, de la siguiente forma:\n Si algún dígito que dice el jugador se encuentra dentro del número a adivinar, pero no está en la posición correcta, se llama PICA.\n Si el dígito se encuentra en la posición adecuada, se llama FIJA.\n Así, las pistas serán la cantidad de PICAS y la cantidad de FIJAS que se tienen.\n El juego termina cuando algún jugador tiene 4 FIJAS (4 dígitos en el orden adecuado).";
 	
 	//genera numero al azar 
 	srand(time(NULL));
@@ -36,6 +36,16 @@ int main(){
 		cin>>numero;
 		numeroIngresadoCompleto[i]=numero;
 	}
+	
+	//Volver a pedir número si el número que se ingreso no es igual al número aleatorio >.<
+	if(numeroIngresadoCompleto[4]!=numeroAleatorio[4])
+		{
+			cout<<"ingresa de nuevo"<<endl;
+			for(int i=0; i<4; i++){
+			cin>>numero;
+			numeroIngresadoCompleto[i]=numero;
+			}
+		}
 	
 	//imprime numero ingresado
 	for(int i=0; i<4; i++){
