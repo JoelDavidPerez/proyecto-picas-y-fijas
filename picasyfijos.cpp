f0df366953948879274d3235f5669401d7c34cf6
JoelDavidPerez <90626632+JoelDavidPerez@users.noreply.github.com>
2021-09-23 19:37:04 -0500

    Update picasyfijos.cpp

--- picasyfijos.cpp
+++ picasyfijos.cpp
@@ -1,59 +1,108 @@
-//Proyecto picas y fijas
 #include <iostream>
 #include <stdlib.h>
+#include <conio.h>
+#include <iomanip>
 #include <time.h>
 
 using namespace std;
 
-int main(){
-	
-	//añadir los carácteres especiales 
-	setlocale(LC_ALL, "spanish");
-	
-	//portada
-	cout<<"PICAS Y FIJAS"<<endl;
-	//cout<<"Tienes la habilidad para completarlo?\nPresiona __ para continuar"  //buscar como poner botones en c++
-	
-	//Instrucciones
-	cout<<"Instrucciones";
-	cout<<"~Un juego muy conocido es picas y fijas~\n\nconsiste en tratar de adivinar un número en la menor cantidad de intentos.\nEn cada intento, el jugador dice 4 dígitos (no repetidos) y el oponente le da pistas de cuántos aciertos tuvo, sin indicarle cuales fueron, de la siguiente forma:\n Si algún dígito que dice el jugador se encuentra dentro del número a adivinar, pero no está en la posición correcta, se llama PICA.\n Si el dígito se encuentra en la posición adecuada, se llama FIJA.\n Así, las pistas serán la cantidad de PICAS y la cantidad de FIJAS que se tienen.\n El juego termina cuando algún jugador tiene 4 FIJAS (4 dígitos en el orden adecuado).";
-	
-	//genera numero al azar 
-	srand(time(NULL));
-	int numeroAleatorio[4];
-	
-	for(int i=0; i<4; i++){
-		numeroAleatorio[i]=rand()%9;
-		cout<<numeroAleatorio[i];
-	}
-	
-	cout<<"\n";
-	
-	//ingresa el numero
-	int numeroIngresadoCompleto[4];
-	int numero;
-	
-	for(int i=0; i<4; i++){
-		cin>>numero;
-		numeroIngresadoCompleto[i]=numero;
-	}
-	
-	//Volver a pedir número si el número que se ingreso no es igual al número aleatorio >.<
-	for(int i=0;numeroIngresadoCompleto[i]!=numeroAleatorio[i];i++)
+int determinarPica(int numeroIngresado, int posicion, int numero[])
+{
+	int devuelve = 0;
+	for(int i=0;i<4;i++)
+	{
+		if(numeroIngresado==numero[i] && posicion!=(i+1))//determina el número que sea igual y que la posición sea igual
 		{
-			cout<<"ingresa de nuevo"<<endl;
-			for(int i=0; i<4; i++){
-			cin>>numero;
-			numeroIngresadoCompleto[i]=numero;
-			}
+			devuelve=1;
+			break;
 		}
+	}
+	return devuelve;
+}
+
+struct Usuario
+{
+	string Nombre;
+	int intentos;
+}datos;
+
+int main()
+{
+	char opcion;
+	do 
+	{
 	
-	//imprime numero ingresado
-	for(int i=0; i<4; i++){
-		cout<<numeroIngresadoCompleto[i];
+	int finalizador=0;
+	int numeroIngresado, unidad,decena,centena,milesima, contadorPicas, contadorFijas,j,temp;
+	int numero[4];
+	system("cls");
+	srand(time(NULL));
+	for(int i=0; i<9; i++){
+		numero[i]=i;
+	}
+	for(int i=0;i<4;i++)
+	{
+		j=(rand()%9);
+		temp=numero[i];
+		numero[i]=numero[j];
+		numero[j]=temp;
+	}
+	for(int i=0;i<4;i++)
+	{
+		cout<<numero[i];
 	}
 	
-	
-	
-	
+	while(finalizador==0){
+		cout<<"Ingresa el numero: "<<endl;
+		cin>>numeroIngresado;
+		while(numeroIngresado>9999)
+		{
+			cout<<"Ingresa de nuevo el número, de 4 cifras"<<endl;
+			cin>>numeroIngresado;
+		}
+		
+		unidad=(numeroIngresado/1000);
+		decena=((numeroIngresado%1000)/100);
+		centena=(((numeroIngresado%1000)%100)/10);
+		milesima=((numeroIngresado%1000)%100)%10;
+		
+		contadorPicas=0;
+		contadorFijas=0;
+		
+		if(unidad==numero[0] && decena==numero[1] && centena==numero[2] && milesima==numero[3]){
+			cout<<"4 fijas. Ganaste";
+			finalizador=1;
+		}
+		else{
+			contadorPicas=contadorPicas + determinarPica(unidad, 1, numero);
+			contadorPicas=contadorPicas + determinarPica(decena, 2, numero);
+			contadorPicas=contadorPicas + determinarPica(centena, 3, numero);
+			contadorPicas=contadorPicas + determinarPica(milesima, 4, numero);
+			
+			if(unidad==numero[0]){
+				contadorFijas=contadorFijas+1;
+			}
+			if(decena==numero[1]){
+				contadorFijas=contadorFijas+1;
+			}
+			if(centena==numero[2]){
+				contadorFijas=contadorFijas+1;
+			}
+			if(milesima==numero[3]){
+				contadorFijas=contadorFijas+1;
+			}
+			
+			cout<<contadorPicas<<"picas,"<<contadorFijas<<"fijas ";
+		}
+		
+	}
+	_getch();
+	//Pregunta para volver a iniciar el proceso	
+		cout << "\n\t\t-------------------------------------------------------" ;
+		cout << "\n\n\n\t>Desea repetir el proceso? S/N" << endl;
+		opcion = getch();	
+	}
+	while (opcion == 'Y' || opcion == 'y' || opcion == 'S' || opcion == 's');
+	cout<<"\n\n\t\t~ Gracias por usar nuestros servicios. Tenga un bonito día ~"<<endl;
+	return 0;
 }
