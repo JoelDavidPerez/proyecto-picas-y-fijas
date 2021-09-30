b1a0e497efda62074c16cc0861f1ef70052f267d
JoelDavidPerez <90626632+JoelDavidPerez@users.noreply.github.com>
2021-09-30 15:35:03 -0500

    Update picasyfijos.cpp

--- picasyfijos.cpp
+++ picasyfijos.cpp
@@ -120,105 +120,105 @@ int main()
 					int termino=0;
 					int num, n1, n2, n3, n4, contadorPicas, contadorFijas,j,temp;
 					int numero[4];
-	int intentos=0;
-	int intentosReinicioFor=0;
+					int intentos=0;
+					int intentosReinicioFor=0;
 
-	
-	srand(time(NULL));
-	for(int i=0; i<9; i++){
-		numero[i]=i;
-	}
-	for(int i=0;i<4;i++)
-	{
-		j=(rand()%9);
-		temp=numero[i];
-		numero[i]=numero[j];
-		numero[j]=temp;
-	}
-	for(int i=0;i<4;i++)
-	{
-		cout<<numero[i];
-	}
-	
-	while(termino==0){
-			cout<<"Ingresa el numero: "<<endl;
-			cin>>num;
-			while(num>9999)
-			{
-				cout<<"Ingresa de nuevo el número, de 4 cifras";
-				cin>>num;
-			}
-			/*while(num<100)
-			{
-				cout<<"Ingresa un valor correcto de 4 cifras por favor: ";
-				cin>> num;
-			}*/
-			while(num<-1)
-			{
-				cout<<"Ingresa un valor correcto. Tiene que ser positivo y de 4 cifras";
-				cin>>num;
-			}
-			n1=(num/1000);
-			n2=((num%1000)/100);
-			n3=(((num%1000)%100)/10);
-			n4=((num%1000)%100)%10;
-			
-			contadorPicas=0;
-			contadorFijas=0;
-			
-			if(n1==numero[0] && n2==numero[1] && n3==numero[2] && n4==numero[3]){
-				cout<<"4 fijas. Ganaste";
-				termino=1;
-			}
-			else if(num==-1)
-			{
-				cout<<"te rendiste :c";
-				termino=1;
-			}
-			else{
-				contadorPicas=contadorPicas + determinarPica(n1, 1, numero);
-				contadorPicas=contadorPicas + determinarPica(n2, 2, numero);
-				contadorPicas=contadorPicas + determinarPica(n3, 3, numero);
-				contadorPicas=contadorPicas + determinarPica(n4, 4, numero);
-				
-				if(n1==numero[0]){
-					contadorFijas=contadorFijas+1;
-				}
-				if(n2==numero[1]){
-					contadorFijas=contadorFijas+1;
-				}
-				if(n3==numero[2]){
-					contadorFijas=contadorFijas+1;
-				}
-				if(n4==numero[3]){
-					contadorFijas=contadorFijas+1;
-				}
-				intentos++;
-				intentosReinicioFor++;
-				cout<<contadorPicas<<"picas,"<<contadorFijas<<"fijas "<<endl;
-				cout<<"intentos: "<<intentos<<endl;
-			}
-			
-			for(int intentosFor=0;intentosReinicioFor==20;intentosFor++)
-			{
-				cout<<"rindete";
-				cin>>num;
-					if(num==-1)
+
+					srand(time(NULL));
+					for(int i=0; i<9; i++){
+						numero[i]=i;
+					}
+					for(int i=0;i<4;i++)
 					{
-					cout<<"te rendiste :c\n";
-					cout<<"El número era: ";
+						j=(rand()%9);
+						temp=numero[i];
+						numero[i]=numero[j];
+						numero[j]=temp;
+					}
 					for(int i=0;i<4;i++)
 					{
 						cout<<numero[i];
 					}
-					termino=1;
+
+					while(termino==0){
+							cout<<"Ingresa el numero: "<<endl;
+							cin>>num;
+							while(num>9999)
+							{
+								cout<<"Ingresa de nuevo el número, de 4 cifras";
+								cin>>num;
+							}
+							/*while(num<100)
+							{
+								cout<<"Ingresa un valor correcto de 4 cifras por favor: ";
+								cin>> num;
+							}*/
+							while(num<-1)
+							{
+								cout<<"Ingresa un valor correcto. Tiene que ser positivo y de 4 cifras";
+								cin>>num;
+							}
+							n1=(num/1000);
+							n2=((num%1000)/100);
+							n3=(((num%1000)%100)/10);
+							n4=((num%1000)%100)%10;
+
+							contadorPicas=0;
+							contadorFijas=0;
+
+							if(n1==numero[0] && n2==numero[1] && n3==numero[2] && n4==numero[3]){
+								cout<<"4 fijas. Ganaste";
+								termino=1;
+							}
+							else if(num==-1)
+							{
+								cout<<"te rendiste :c";
+								termino=1;
+							}
+							else{
+								contadorPicas=contadorPicas + determinarPica(n1, 1, numero);
+								contadorPicas=contadorPicas + determinarPica(n2, 2, numero);
+								contadorPicas=contadorPicas + determinarPica(n3, 3, numero);
+								contadorPicas=contadorPicas + determinarPica(n4, 4, numero);
+
+								if(n1==numero[0]){
+									contadorFijas=contadorFijas+1;
+								}
+								if(n2==numero[1]){
+									contadorFijas=contadorFijas+1;
+								}
+								if(n3==numero[2]){
+									contadorFijas=contadorFijas+1;
+								}
+								if(n4==numero[3]){
+									contadorFijas=contadorFijas+1;
+								}
+								intentos++;
+								intentosReinicioFor++;
+								cout<<contadorPicas<<"picas,"<<contadorFijas<<"fijas "<<endl;
+								cout<<"intentos: "<<intentos<<endl;
+							}
+
+							for(int intentosFor=0;intentosReinicioFor==20;intentosFor++)
+							{
+								cout<<"rindete";
+								cin>>num;
+									if(num==-1)
+									{
+									cout<<"te rendiste :c\n";
+									cout<<"El número era: ";
+									for(int i=0;i<4;i++)
+									{
+										cout<<numero[i];
+									}
+									termino=1;
+									}
+								intentosReinicioFor=0;
+								intentosFor=0;
+							}
 					}
-				intentosReinicioFor=0;
-				intentosFor=0;
-			}
-	}
-	_getch();
-}
+					_getch();
+				}
 			}
 		}
 		//Pregunta para volver a iniciar el proceso	
